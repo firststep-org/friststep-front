3981fa1e68117cb865f18d86e6c2c49ee99d6c28
peppone-choi <peppone.choi@gmail.com>
2024-04-10 21:19:03 +0900

    type 일부 수정

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -4,8 +4,9 @@ import React from "react";
 import { ThemeProvider } from "@material-tailwind/react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { StyledEngineProvider } from "@mui/material";
+import { ReactNodeLike } from "prop-types";
 
-const Provider = ({ children }: { children: React.ReactNode }) => {
+const Provider = ({ children }: { children: ReactNodeLike }) => {
   return (
     <>
       <StyledEngineProvider injectFirst>
